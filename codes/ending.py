92c5b11188787d286ead82ace873317de3490941
jsdhwdmaL <yizhou_li20060430@163.com>
2025-02-08 14:21:47 -0500

    some keyboard changes

--- codes/ending.py
+++ codes/ending.py
@@ -11,12 +11,13 @@ import level2
 def page_end(screen, player, WIDTH, HEIGHT):
     # background
     font = pygame.font.Font(None, 60)
+    font2 = pygame.font.Font(None, 36)
     background = Sprites.StoneBackground(WIDTH, HEIGHT) # Adjust path if needed
 
     # Initialize puzzle scene
-    puzzle_text = "Congrats! You've escaped!"
+    congrats_text = "Congrats! You've escaped!"
     instruction_text = "Press Space to exit the game"
-    lines = level2.wrap_text(puzzle_text, font, WIDTH - 20)
+    lines = level2.wrap_text(instruction_text, font, WIDTH - 20)
     player_input = ""
     solved = False
     foreground = Sprites.ForeGround(screen)
@@ -24,6 +25,9 @@ def page_end(screen, player, WIDTH, HEIGHT):
     submitted = False
 
     while running:
+        # get keyboard input
+        keys = pygame.key.get_pressed()
+
         # Different background color for the new page
         background.draw(screen)
 
@@ -32,20 +36,20 @@ def page_end(screen, player, WIDTH, HEIGHT):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.key == pygame.K_SPACE:
+            if keys[pygame.K_SPACE]:
                 pygame.quit()
                 sys.exit()
 
         # Render puzzle text
-        y_offset = 50  # Start position for the text
+        y_offset = 500  # Start position for the text
         for line in lines:
-            rendered_text = font.render(line, True, (255, 255, 255))  # Black text
+            rendered_text = font2.render(line, True, (255, 255, 255))  # Black text
             screen.blit(rendered_text, (30, y_offset))  # Draw at position (10, y_offset)
             y_offset += rendered_text.get_height()  # Move to the next line
 
         # Display feedback
-        solved_surface = font.render(instruction_text, True, (255, 255, 0))
-        screen.blit(solved_surface, (100, 300))
+        solved_surface = font.render(congrats_text, True, (255, 255, 0))
+        screen.blit(solved_surface, (100, 250))
 
         foreground.draw()
         # Update the display
