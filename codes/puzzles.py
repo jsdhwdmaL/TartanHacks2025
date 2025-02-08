3231b04fa16696aba8c67f90d6f1ec4e58570e71
Hashim Alsharaf <halsharaf23@gmail.com>
2025-02-07 23:15:17 -0500

    ACTUALLY starting work on puzzle 1

---
+++ codes/puzzles.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.locals import *
+
+class Puzzle1:
+    def __init__(self):
+        self.image = pygame.image.load("assets/key.png")
+        self.x = 380
+        self.y = 300
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
+        self.playerHasKey = False
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    
+    def checkTouch(self, player):
+        #rect
+        collide = pygame.Rect.collidepoint() #check if collided with player
+        #make the key disappear
+        #now the player should move to the chest or the other target to solve this puzzle
+
